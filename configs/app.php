38f13f9371277df0efe13905a55187cc00cb82a4
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-11-29 09:42:07 +0330

    init

--- configs/app.php
+++ configs/app.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_NAME', 'Mvc Freamwork');
+define('APP_NAME', 'Mvc Framework');
 define('APP_PORT', 8000);
 define('APP_URL', "http://$_SERVER[HTTP_HOST]:".APP_PORT);
 define('BASE_DIR', dirname(__DIR__));
