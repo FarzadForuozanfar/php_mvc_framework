c3467232766c8ba6b6a0a7eb0feb8a33a886b317
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-11-30 20:31:11 +0330

    add validation

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -10,10 +10,22 @@ class RegisterModel extends BaseModel
     public string $lastname;
     public string $email;
     public string $password;
-    public string $passwordConfirm;
+    public string $confirmPassword;
 
     public function register()
     {
         echo "creting new user";
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8]],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
     }
 }
--- views/register.php
+++ views/register.php
@@ -5,27 +5,47 @@
         <div class="col">
             <div class="form-group">
                 <label for="firstname">First name</label>
-                <input id="firstname" type="text" class="form-control" name="firstname">
+                <input id="firstname" type="text" name="firstname" value="<?= $model->firstname; ?>"
+                       class="form-control <?= $model->hasError('firstname') ? 'is-invalid' : '' ?>">
+                <div class="invalid-feedback">
+                    <?= $model->getFirstError('firstname'); ?>
+                </div>
             </div>
         </div>
         <div class="col">
             <div class="form-group">
                 <label for="lastname">Last name</label>
-                <input id="lastname" type="text" class="form-control" name="lastname">
+                <input id="lastname" type="text" name="lastname" value="<?= $model->lastname; ?>"
+                       class="form-control <?= $model->hasError('lastname') ? 'is-invalid' : '' ?>">
+                <div class="invalid-feedback">
+                    <?= $model->getFirstError('lastname'); ?>
+                </div>
             </div>
         </div>
     </div>
     <div class="form-group mb-3">
         <label for="email">Email address</label>
-        <input id="email" type="email" class="form-control" name="email">
+        <input id="email" type="email" name="email" value="<?= $model->email; ?>"
+               class="form-control <?= $model->hasError('email') ? 'is-invalid' : '' ?>">
+        <div class="invalid-feedback">
+            <?= $model->getFirstError('email'); ?>
+        </div>
     </div>
     <div class="form-group mb-3">
         <label for="password">Password</label>
-        <input id="password" type="password" class="form-control" name="password">
+        <input id="password" type="password" name="password" value="<?= $model->password; ?>"
+               class="form-control <?= $model->hasError('password') ? 'is-invalid' : '' ?>">
+        <div class="invalid-feedback">
+            <?= $model->getFirstError('password'); ?>
+        </div>
     </div>
     <div class="form-group mb-3">
         <label for="confirm-password">Confirm Password</label>
-        <input id="confirm-password" type="password" class="form-control" name="confirmPassword">
+        <input id="confirm-password" type="password" name="confirmPassword" value="<?= $model->confirmPassword; ?>"
+               class="form-control <?= $model->hasError('confirmPassword') ? 'is-invalid' : '' ?>">
+        <div class="invalid-feedback">
+            <?= $model->getFirstError('confirmPassword'); ?>
+        </div>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
