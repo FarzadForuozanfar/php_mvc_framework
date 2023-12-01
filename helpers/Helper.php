76e46bb6ba4956e7f8e8e3e0e6433685ba6234bb
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-12-01 15:32:16 +0330

    add helpers

---
+++ helpers/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\helpers;
+
+class Helper
+{
+    public static function bcrypt($data): string
+    {
+        return hash('sha256', uniqid() . md5($data) . time()) . md5(uniqid() . md5($data) . time());
+    }
+}
