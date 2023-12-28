ab94006e2bbe63d43b02dca13aac830c95dba109
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-12-28 11:13:30 +0330

    add timezone cfg

--- config/app.php
+++ config/app.php
@@ -4,3 +4,4 @@ define('APP_NAME', 'Mvc Framework');
 define('APP_PORT', 8000);
 define('APP_URL', "http://$_SERVER[HTTP_HOST]");
 define('BASE_DIR', dirname(__DIR__));
+define('TIMEZONE', 'Asia/Tehran');
