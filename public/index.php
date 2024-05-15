fc97fe67b8ed68bfc5e5f20b8b8197db182884fd
alirezajawadi <alirezajavadigit@gmail.com>
2024-05-15 23:20:34 +0330

    Update index.php
    Updated index.php to load environment variables with Dotenv and enable error display based on APP_DEBUG setting.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,22 @@ use App\controllers\AuthController;
 use App\controllers\SiteController;
 use Core\Application;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Create a new instance of Dotenv with immutable settings, specifying the directory containing the .env file.
+$dotenv = Dotenv\Dotenv::createImmutable("../");
+
+// Load the environment variables from the .env file into the application.
+$dotenv->load();
+
+// Check if the environment variable "APP_DEBUG" is set to true and strictly compare its value.
+if (env("APP_DEBUG") === true) {
+    // If "APP_DEBUG" is true, enable displaying of errors on the screen.
+    ini_set('display_errors', 'on');
+
+    // Set error reporting to display all types of errors for better debugging.
+    error_reporting(E_ALL);
+}
 
 date_default_timezone_set(config('TIMEZONE'));
 
