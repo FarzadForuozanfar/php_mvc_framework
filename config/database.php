8974493ab90774999a3e000346f4a7b2f6568cd0
kourosh khalily <152788866+TheKouroshKhalili@users.noreply.github.com>
2025-08-07 15:25:52 +0330

    fix(docker): fix some bugs

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_HOST", "127.0.0.1");
-define("DB_NAME", "mvc");
-define("DB_PASS", "");
-define("DB_USER", "root");
+define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
+define('DB_NAME', getenv('DB_DATABASE') ?: 'mvc');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASSWORD') ?: '');
