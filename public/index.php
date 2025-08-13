c58adaa32848625aec9179dfc99612c8c3c5eedb
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2025-08-13 20:38:30 +0330

    feat: dockerized application

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ use Core\Application;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 date_default_timezone_set(config('TIMEZONE'));
 
 $app = new Application();
